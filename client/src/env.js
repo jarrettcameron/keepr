9a05520fdbe7f21d60c0e420a474364f615b7fa5
Jarrett Cameron <native@hotmail.com>
2025-04-04 16:54:10 -0600

    deploy

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7045' : 'https://keepr.jarrettcameron.com'
 export const useSockets = false
 export const domain = 'dev-rl8038aaudw3trob.us.auth0.com'
 export const clientId = 'H0dqgRYCRdtyN24QvvlyPuo2FZahpeOp'
